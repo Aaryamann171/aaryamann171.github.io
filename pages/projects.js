8a82b6f88dae1baf2cb34c4d4512be24b2a6341c
Aaryamann171 <aaryamann171@gmail.com>
2021-10-05 10:58:39 +0530

    renamed components files to get around GH actions issue

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,6 @@
 import Project from "../components/Project";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 const Projects = () => {
   return (
--- pages/skills.js
+++ pages/skills.js
@@ -1,6 +1,6 @@
 import Skill from "../components/Skill";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
 
 const skillsCardStyle = {
   border: "1px solid black",
