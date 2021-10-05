343f5b3fe791a2b0d7107149df8e2a396b611f83
Aaryamann171 <aaryamann171@gmail.com>
2021-10-05 10:15:01 +0530

    fixed imports

--- pages/projects.js
+++ pages/projects.js
@@ -1,6 +1,6 @@
 import Project from "../components/Project";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Projects = () => {
   return (
