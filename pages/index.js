56d1c936482d829ed693b4acaf99f95a3bcd98dc
aaryamann171 <aaryamann171@gmail.com>
2022-12-03 13:45:47 +0530

    added next Image to home page and changed text ordering

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
+import Image from "next/image";
 
 const foobar = {
   backgroundColor: "#0d1117",
@@ -29,10 +30,12 @@ export default function Home() {
             <div>
               <div className="row">
                 <div className="col-md-4 col-sm-12">
-                  <img
+                  <Image
                     src="/images/others/aaryamann_vector.png"
                     alt="aaryamann"
                     style={imgStyle}
+                    height={400}
+                    width={400}
                   />
                 </div>
                 <div className="col-md-8 col-sm-12">
@@ -42,12 +45,12 @@ export default function Home() {
                     <hr />
                   </h1>
                   <p className="lead">
-                    B.Tech in <strong>Computer Science</strong> with
+                    I like to play around with various technologies and
+                    collaborate with like minded people. <br />
+                    I am currently working as a <strong>Full Stack Developer</strong>.
+                    I have a B.Tech in <strong>Computer Science</strong> with
                     specialization in <strong>A</strong>rtificial{" "}
                     <strong>I</strong>ntelligence. <br />
-                    Currently working as a <strong>Full Stack Developer</strong>
-                    . <br />I like to play around with various technologies and
-                    collaborate with like minded people.
                   </p>
                 </div>
               </div>
