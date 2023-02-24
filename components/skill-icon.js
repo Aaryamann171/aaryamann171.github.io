2cd621521186b67e568bf04ee5a19bc1402ce399
aaryamann171 <aaryamann171@gmail.com>
2023-02-25 01:34:47 +0530

    reverted back skill icon changes

--- components/skill-icon.js
+++ components/skill-icon.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const SkillIcon = ({ thumbnailSrc, title }) => {
   return (
-    <div className="bg-white rounded-full border-2 p-2 flex items-center">
+    <div className="flex items-center">
       <Image
         alt={title}
         src={thumbnailSrc}
         title={title}
-        height={35}
-        width={35}
+        height={50}
+        width={50}
         loading="lazy"
       />
     </div>
